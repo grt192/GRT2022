b01a869fa6e6e313a9ef5529dccb97da1f69e19d
Kevin Yu <kevinyu567@gmail.com>
2021-11-07 23:23:50 -0800

    Revert "Add third set of Talons"
    This reverts commit 1b5ea6d2a5522477e4151abe8c168437a8f0d1df.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,13 +16,10 @@ package frc.robot;
  */
 public final class Constants {
     public static final class TankConstants {
+        public static final int fLeftMotorPort = 3;
         public static final int fRightMotorPort = 1;
-        public static final int mRightMotorPort = 2;
-        public static final int bRightMotorPort = 3;
-
-        public static final int fLeftMotorPort = 4;
-        public static final int mLeftMotorPort = 5;
-        public static final int bLeftMotorPort = 6;
+        public static final int bLeftMotorPort = 4;
+        public static final int bRightMotorPort = 2;
     }
 
     public static final class ElevatorConstants {
