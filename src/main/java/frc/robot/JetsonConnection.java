6836c9359ac88143403041495cb68adc9f4be296
pinkbluesky <pinkbluesky@users.noreply.github.com>
2021-12-22 17:43:09 -0800

    Add Jetson NT testing code

--- src/main/java/frc/robot/JetsonConnection.java
+++ src/main/java/frc/robot/JetsonConnection.java
@@ -12,13 +12,34 @@ public class JetsonConnection {
         table = NetworkTableInstance.getDefault().getTable(TABLE_NAME);
     }
 
-    public void run() {
+    /**
+     * Runs periodically in a Thread. Put print lines in here or whatever else you
+     * want.
+     */
+    public void periodic() {
 
+        System.out.println("test: " + getString("test"));
+        System.out.println("xCentroids len: " + getDoubleArray("xCentroids").length);
+        System.out.println("yCentroids len: " + getDoubleArray("yCentroids").length);
+    }
+
+    public String getString(String key) {
+        return table.getEntry(key).getString("");
     }
 
     public double getDouble(String key) {
         System.out.println("/" + TABLE_NAME + "/" + key + " = " + table.getEntry(key).getDouble(0));
         return table.getEntry(key).getDouble(0);
+    }
+
+    public double[] getDoubleArray(String key) {
+        Number[] numArr = table.getEntry(key).getNumberArray(new Number[0]);
+
+        double[] arr = new double[0];
+        for (int i = 0; i < numArr.length; i++) {
+            arr[i] = numArr[i].doubleValue();
+        }
+        return arr;
     }
 
 }
