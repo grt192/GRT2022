80c540f540c5244a00c1249da52f25d9bc1eb674
Kevin Yu <kevinyu567@gmail.com>
2022-01-23 17:09:30 -0800

    PID testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
 
     public static final class ShooterConstants {
         public static final int turntablePort = 0;
-        public static final int flywheelPort = 1;
-        public static final int hoodPort = 2;
+        public static final int hoodPort = 1;
+        public static final int flywheelPort = 2;
     }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.shooter;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -22,10 +25,16 @@ public class ShooterSubsystem extends SubsystemBase {
 
   private final CANSparkMax hood;
   private final RelativeEncoder hoodEncoder;
+  private final SparkMaxPIDController hoodPidController;
 
   private final CANSparkMax flywheel;
   private final RelativeEncoder flywheelEncoder;
 
+  private final double kP = 0.125;
+  private final double kI = 0;
+  private final double kD = 0;
+
+  private boolean isOpen = false;
   private double lastUpdate = 0;
   private double lastPos = 0;
 
@@ -35,20 +44,35 @@ public class ShooterSubsystem extends SubsystemBase {
   private NetworkTableEntry sVelo = sTab.add("velo", 1337).getEntry();
 
   public ShooterSubsystem() {
+    // Initialize turntable Talon and encoder PID
     turntable = new WPI_TalonSRX(turntablePort);
     turntable.configFactoryDefault();
     turntable.setNeutralMode(NeutralMode.Brake);
-    turntable.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
 
+    turntable.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+    turntable.setSensorPhase(false);
+    turntable.config_kP(0, kP);
+    turntable.config_kI(0, kI);
+    turntable.config_kD(0, kD);
+
+    // Initialize hood SparkMax and encoder PID
     hood = new CANSparkMax(hoodPort, MotorType.kBrushless);
     hood.restoreFactoryDefaults();
     hood.setIdleMode(IdleMode.kBrake);
+
     hoodEncoder = hood.getEncoder();
     hoodEncoder.setPosition(0);
 
+    hoodPidController = hood.getPIDController();
+    hoodPidController.setP(kP);
+    hoodPidController.setI(kI);
+    hoodPidController.setD(kD);
+
+    // Initialize flywheel SparkMax
     flywheel = new CANSparkMax(flywheelPort, MotorType.kBrushless);
     flywheel.restoreFactoryDefaults();
     //flywheel.setIdleMode(IdleMode.kBrake);
+
     flywheelEncoder = flywheel.getEncoder();
     flywheelEncoder.setPosition(0);
   }
@@ -56,20 +80,6 @@ public class ShooterSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // TODO: implement vision tracking and turntable
-
-    double time = Timer.getFPGATimestamp();
-    double dTime = time - lastUpdate;
-
-    double power = targetPower.getDouble(0.8);
-    flywheel.set(power);
-
-    double pos = flywheelEncoder.getPosition();
-    sPos.setDouble(pos);
-
-    double velo = (pos - lastPos) / dTime;
-    sVelo.setDouble(velo);
-
-    lastPos = pos;
   }
 
   /**
@@ -77,5 +87,17 @@ public class ShooterSubsystem extends SubsystemBase {
    */
   public void shoot() {
     // TODO: implement this
+
+    System.out.println(isOpen);
+    hoodPidController.setReference(isOpen ? 5 : -5, ControlType.kPosition);
+    //turntable.set(ControlMode.Position, isOpen ? 1000 : -1000);
+    isOpen = !isOpen;
+  }
+
+  public void test() {
+    System.out.println(isOpen);
+    hood.set(!isOpen ? 0.5 : 0);
+    //turntable.set(ControlMode.PercentOutput, !isOpen ? 0.5 : 0);
+    isOpen = !isOpen;
   }
 }
