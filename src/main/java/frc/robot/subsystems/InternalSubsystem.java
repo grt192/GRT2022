0247322e58b0f221d09bd2e25704e94c2c602d6e
Ippyb <itzmeipsita@gmail.com>
2022-01-24 19:37:52 -0800

    shooter request updates

--- src/main/java/frc/robot/subsystems/InternalSubsystem.java
+++ src/main/java/frc/robot/subsystems/InternalSubsystem.java
@@ -12,10 +12,17 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
 
 import static frc.robot.Constants.InternalConstants.*;
+import frc.robot.jetson.JetsonConnection;
+
+import frc.robot.subsystems.TurretSubsystem;
 
 public class InternalSubsystem extends SubsystemBase {
 
+  private final TurretSubsystem turretSubsystem;
+
   private final WPI_TalonSRX motor;
+
+  private final JetsonConnection jetson;
 
   private final ColorSensorV3 sensorTop;
   private final ColorSensorV3 sensorBottom;
@@ -30,9 +37,16 @@ public class InternalSubsystem extends SubsystemBase {
   private final Color BlueTarget = new Color(0.143, 0.427, 0.429);
   private final Color RedTarget = new Color(0.561, 0.232, 0.114);
 
-  String color;
+  private final String color;
+  
+  private final boolean shotRequested;
 
   public InternalSubsystem() {
+
+    jetson = new JetsonConnection();
+
+    TurretSubsystem turretSubsystem = new TurretSubsystem(jetson);
+
     motor = new WPI_TalonSRX(motorPort);
 
     /**
@@ -52,11 +66,17 @@ public class InternalSubsystem extends SubsystemBase {
      * with given confidence range.
      */
     colorMatcher = new ColorMatch();
+    shotRequested = false;
   }
 
   @Override
   public void periodic() {
     controlFeed();
+
+    if (turretSubsystem.flywheelReady() && shotRequested){ 
+      // also add turntable aligned condition
+      //TODO launch ball into turret
+    }
   }
 
   public boolean isRed(ColorSensorV3 s) {
@@ -79,5 +99,9 @@ public class InternalSubsystem extends SubsystemBase {
         motor.set(ControlMode.PercentOutput, .5);
       }
     }
+  }
+
+  public void requestShot() {
+    this.shotRequested = true;
   }
 }
