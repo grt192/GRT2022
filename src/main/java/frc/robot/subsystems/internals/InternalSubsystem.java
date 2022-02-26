e4e69e241fa023839dff8b3268c7ea3bf7b9bdac
eggaskin <evelyngaskin@gmail.com>
2022-02-25 16:11:48 -0800

    final (probably) internals code!

--- src/main/java/frc/robot/subsystems/internals/InternalSubsystem.java
+++ src/main/java/frc/robot/subsystems/internals/InternalSubsystem.java
@@ -40,12 +40,10 @@ public class InternalSubsystem extends GRTSubsystem {
     private final ColorMatch colorMatcher;
 
     private final Timer exitTimer;
-    //private final Timer entranceTimer;
     private final Timer storageTimer;
 
     private int ballCount = 0;
 
-    private boolean prevEntranceDetected = false;
     private boolean entToStorage = false;
 
     private boolean shotRequested = false;
@@ -94,21 +92,12 @@ public class InternalSubsystem extends GRTSubsystem {
             default: ALLIANCE_COLOR = RED; break;
         }
 
-        // TODO: it probably isn't great to do this in the constructor? probably hard code the ball count
-        Color storageColor = matchColor(colorSensorThread.getLastStorage());
-        boolean storageDetected = isBall(storageColor);
-        boolean stagingDetected = staging.get() >= 0.2;
-        ballCount = (stagingDetected ? 1 : 0) + (storageDetected ? 1 : 0);
     }
 
     @Override
     public void periodic() {
         // If a new ball has entered internals, increment ball count
-        // TODO: either we keep this or use storage and staging to set ball count; currently this is redundant
         boolean entranceDetected = entrance.get() >= 0.4;
-        if (!prevEntranceDetected && entranceDetected) ballCount++;
-        prevEntranceDetected = entranceDetected;
-
         Color storageColor = matchColor(colorSensorThread.getLastStorage());
         boolean storageDetected = isBall(storageColor);
 
@@ -120,18 +109,13 @@ public class InternalSubsystem extends GRTSubsystem {
         if (driverOverride) return;
 
         // If there is a ball in the entrance, run the bottom motor.
-        if (prevEntranceDetected && !storageDetected) {
-            // Spin the bottom motor on a timer
-            //entranceTimer.start();
+        if (entranceDetected && !storageDetected) {
             motorBottom.set(0.3);
             entToStorage = true;
         }
 
         // If 1.5 second has elapsed and ball has progressed past entrance, stop motor
-        //(entranceTimer.get() >= 1.5) && 
         if (storageDetected && entToStorage) {
-            //entranceTimer.stop();
-            //entranceTimer.reset();
             motorBottom.set(0);
             entToStorage = false;
         }
@@ -162,15 +146,16 @@ public class InternalSubsystem extends GRTSubsystem {
 
         if (turretSubsystem != null) {
             // If a shot was requested and the turret is ready, load a ball into the turret.
-            // TODO: why do we no longer check turret for alignment before shooting?
             turretSubsystem.setReject(rejecting);
-            if (shotRequested) {
+            if (shotRequested /* && turretSubsystem.getState() == TurretSubsystem.ModuleState.HIGH_TOLERANCE
+            || rejecting && turretSubsystem.getState() == TurretSubsystem.ModuleState.LOW_TOLERANCE */) {
+               
                 // Spin the top motor on a timer
                 exitTimer.start();
                 motorTop.set(0.5);
 
-                // If 1.5 seconds have elapsed, mark the shot as finished
-                if (exitTimer.hasElapsed(1)) {
+                // If 0.5 seconds have elapsed, mark the shot as finished
+                if (exitTimer.hasElapsed(0.5)) {
                     exitTimer.stop();
                     exitTimer.reset();
                     motorTop.set(0);
@@ -178,12 +163,7 @@ public class InternalSubsystem extends GRTSubsystem {
                     // Reset states
                     shotRequested = false;
                     rejectingChecked = false;
-
-                    //TODO one problem here: if we request a shot without a ball being output
-                    //our ball count will be off
-                    ballCount--;
-                    System.out.println("ball exited turret, was" + rejecting);
-                }  
+                }
             }
         }
     }
